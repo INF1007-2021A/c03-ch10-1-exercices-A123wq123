feb7b985386c9c148f8d6fb7673ea99d8878e351
A123wq123 <charles.degrandpre@polymtl.ca>
2021-11-08 14:36:40 -0500

    Exercices chap 10 partie 1 version 1(numéros 1 finis et 2 on the way)

--- exercice.py
+++ exercice.py
@@ -7,11 +7,14 @@ import numpy as np
 
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
+
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.array([np.random.uniform(-1.3, 2.5, 64)])
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
+    for couple_x_y in cartesian_coordinates:
+
     return np.array([])
 
 
@@ -21,5 +24,6 @@ def find_closest_index(values: np.ndarray, number: float) -> int:
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    print(linear_values())
 
     pass
